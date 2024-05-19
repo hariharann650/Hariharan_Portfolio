eecc771e9d442fc6cde14ff1458beccbdca74202
HARIHARAN P <114478496+hariharann650@users.noreply.github.com>
2024-05-19 16:13:22 +0530

    change the link of work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -5,10 +5,10 @@
 // import project5_img from '../assets/project_5.svg'
 // import project6_img from '../assets/project_6.svg'
 
-import project1_img from '../assets/project1.png'
-import project2_img from '../assets/project2.png'
-import project3_img from '../assets/project3.png'
-import project4_img from '../assets/project4.png'
+import project1_img from '../assets/project2.png'
+import project2_img from '../assets/project3.png'
+import project3_img from '../assets/project4.png'
+import project4_img from '../assets/project1.png'
 // import project5_img from '../assets/project_5.svg'
 // import project6_img from '../assets/project_6.svg'
 
@@ -19,25 +19,26 @@ const mywork_data = [
         w_no:1,
         w_name:"Web design",
         w_img:project1_img,
-        link:'https://hariharann650.github.io/SubscriptionProject/'
+        link:'https://hariharann650.github.io/HariCarShowroom/index.html'
+        
     },
     {
         w_no:2,
         w_name:"Web design",
         w_img:project2_img,
-        link:'https://hariharann650.github.io/HariCarShowroom/index.html'
+        link:'https://hariharann650.github.io/youtubenew/'
     },
     {
         w_no:3,
         w_name:"Web design",
         w_img:project3_img,
-        link:'https://hariharann650.github.io/youtubenew/'
+        link:'https://hariharann650.github.io/SubscriptionProject/'
     },
     {
         w_no:4,
         w_name:"Web design",
         w_img:project4_img,
-        link:'https://hariharann650.github.io/SubscriptionProject/'
+        link:'https://hariharann650.github.io/shoe_project/'
     },
     // {
     //     w_no:5,
