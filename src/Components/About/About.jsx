874407462671fd2f128cb2fffefb7327104847b1
HARIHARAN P <114478496+hariharann650@users.noreply.github.com>
2024-06-11 07:55:09 +0530

    about section paragraph animation

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./About.css";
-import hari from '../../assets/file.png';
-import theme from '../../assets/theme_pattern.svg';
+import hari from "../../assets/file.png";
+import theme from "../../assets/theme_pattern.svg";
 
 const About = () => {
   return (
-    <div id="about" className="about"> 
+    <div id="about" className="about">
       <div className="about-title">
         <h1>About me</h1>
         <img src={theme} alt="" />
@@ -17,21 +17,45 @@ const About = () => {
         <div className="about-right">
           <div className="about-para">
             <p>
-            Hi, I'm Hariharan , a passionate Java full stack developer with interest in building robust and scalable web applications. 
-            With a solid foundation in Java programming and expertise in front-end and back-end development technologies, I thrive on tackling complex
-             challenges and delivering elegant solutions that exceed expectations.
+              <span>
+                Hi, I'm Hariharan , a passionate Java full stack developer with
+                interest in building robust and scalable web applications. With
+                a solid foundation in Java programming and expertise in
+                front-end and back-end development technologies, I thrive on
+                tackling complex challenges and delivering elegant solutions
+                that exceed expectations.
+              </span>
             </p>
             <p>
-            On the front-end, I am skilled in HTML, CSS, and JavaScript, with additional expertise in modern libraries
-             like React. Additionally, I have experience with database management systems such as MySQL
+              <span>
+                On the front-end, I am skilled in HTML, CSS, and JavaScript,
+                with additional expertise in modern libraries like React.
+                Additionally, I have experience with database management systems
+                such as MySQL
+              </span>
             </p>
           </div>
           <div className="about-skills">
-            <div className="about-skill"><p>HTML & CSS</p><hr className="percentage1" style={{width:'80%'}} /></div>
-            <div className="about-skill"><p>Javascript</p><hr className="percentage2" style={{width:'60%'}} /></div>
-            <div className="about-skill"><p>Vite + React Js</p><hr className="percentage3" style={{width:'50%'}} /></div>
-            <div className="about-skill"><p>Java</p><hr className="percentage4" style={{width:'60%'}} /></div>
-            <div className="about-skill"><p>My SQL</p><hr className="percentage5" style={{width:'70%'}} /></div>
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr className="percentage1" style={{ width: "80%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Javascript</p>
+              <hr className="percentage2" style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Vite + React Js</p>
+              <hr className="percentage3" style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Java</p>
+              <hr className="percentage4" style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>My SQL</p>
+              <hr className="percentage5" style={{ width: "70%" }} />
+            </div>
           </div>
         </div>
       </div>
